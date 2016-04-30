d4ce91d06f30263636ca8d39ee40d06f46a5004b
Dwayne Jeng <dwayne.jeng@sagebase.org>
2016-04-29 18:14:09 -0700

    tests for new V4 Schema fields and Schema version conflicts

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/UploadSchemaTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/UploadSchemaTest.java
@@ -5,14 +5,19 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.joda.time.DateTime;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.sagebionetworks.bridge.sdk.DeveloperClient;
 import org.sagebionetworks.bridge.sdk.Roles;
+import org.sagebionetworks.bridge.sdk.exceptions.ConcurrentModificationException;
 import org.sagebionetworks.bridge.sdk.exceptions.EntityNotFoundException;
 import org.sagebionetworks.bridge.sdk.exceptions.UnauthorizedException;
 import org.sagebionetworks.bridge.sdk.models.ResourceList;
@@ -22,17 +27,33 @@ import org.sagebionetworks.bridge.sdk.models.upload.UploadSchema;
 import org.sagebionetworks.bridge.sdk.models.upload.UploadSchemaType;
 
 public class UploadSchemaTest {
-    private static final Logger LOG = LoggerFactory.getLogger(UploadSchemaTest.class);
-
     private static final String TEST_SCHEMA_ID_PREFIX = "integration-test-schema-";
 
     private static TestUserHelper.TestUser developer;
+    private static DeveloperClient developerClient;
     private static TestUserHelper.TestUser user;
+
+    private String schemaId;
 
     @BeforeClass
     public static void beforeClass() {
         developer = TestUserHelper.createAndSignInUser(UploadSchemaTest.class, false, Roles.DEVELOPER);
+        developerClient = developer.getSession().getDeveloperClient();
         user = TestUserHelper.createAndSignInUser(UploadSchemaTest.class, true);
+    }
+
+    @Before
+    public void before() {
+        schemaId = TEST_SCHEMA_ID_PREFIX + RandomStringUtils.randomAlphabetic(4);
+    }
+
+    @After
+    public void deleteSchemas() {
+        try {
+            developerClient.deleteUploadSchemaAllRevisions(schemaId);
+        } catch (EntityNotFoundException ex) {
+            // Suppress the exception, as the test may have already deleted the schema.
+        }
     }
 
     @AfterClass
@@ -51,10 +72,6 @@ public class UploadSchemaTest {
 
     @Test
     public void test() {
-        DeveloperClient developerClient = developer.getSession().getDeveloperClient();
-        String schemaId = TEST_SCHEMA_ID_PREFIX + RandomStringUtils.randomAlphabetic(4);
-        LOG.info("schemaId=" + schemaId);
-
         // set up some field defs
         UploadFieldDefinition fooFieldDef = new UploadFieldDefinition.Builder().withName("foo").withRequired(true)
                 .withType(UploadFieldType.STRING).build();
@@ -128,7 +145,6 @@ public class UploadSchemaTest {
     }
 
     private static UploadSchema createOrUpdateSchemaAndVerify(UploadSchema schema) {
-        DeveloperClient developerClient = developer.getSession().getDeveloperClient();
         UploadSchema returnedSchema = developerClient.createOrUpdateUploadSchema(schema);
 
         // all fields should match, except revision which is incremented
@@ -144,6 +160,85 @@ public class UploadSchemaTest {
         }
 
         return returnedSchema;
+    }
+
+    @Test
+    public void optionalFields() {
+        // We test every field except DDB version, as DDB version is (from the perspective of the Bridge client), an
+        // opaque token that should be passed back to the server on update.
+
+        // Server stores surveyCreatedOnMillis as epoch milliseconds, so we can't directly compare date-times.
+        final DateTime surveyCreatedOn = DateTime.parse("2016-04-29T16:00:00.002-0700");
+        final long surveyCreatedOnMillis = surveyCreatedOn.getMillis();
+
+        // Create schema with all the fields.
+        UploadFieldDefinition fieldDef = new UploadFieldDefinition.Builder().withFileExtension(".test")
+                .withMimeType("text/plain").withMinAppVersion(1).withMaxAppVersion(37).withMaxLength(24)
+                .withName("field").withRequired(false).withType(UploadFieldType.STRING).build();
+        List<UploadFieldDefinition> fieldListList = ImmutableList.of(fieldDef);
+        UploadSchema schema = new UploadSchema.Builder().withFieldDefinitions(fieldListList).withName("Schema")
+                .withSchemaId(schemaId).withSchemaType(UploadSchemaType.IOS_SURVEY).withSurveyGuid("survey")
+                .withSurveyCreatedOn(surveyCreatedOn).build();
+        UploadSchema createdSchema = developerClient.createOrUpdateUploadSchema(schema);
+
+        assertEquals(fieldListList, createdSchema.getFieldDefinitions());
+        assertEquals("Schema", createdSchema.getName());
+        assertEquals(1, createdSchema.getRevision().intValue());
+        assertEquals(schemaId, createdSchema.getSchemaId());
+        assertEquals(UploadSchemaType.IOS_SURVEY, createdSchema.getSchemaType());
+        assertEquals("survey", createdSchema.getSurveyGuid());
+        assertEquals(surveyCreatedOnMillis, createdSchema.getSurveyCreatedOn().getMillis());
+    }
+
+    @Test
+    public void newSchemaVersionConflict() {
+        testVersionConflict(null, null);
+    }
+
+    @Test
+    public void createSchemaVersionConflict() {
+        // Create the schema first.
+        developerClient.createOrUpdateUploadSchema(makeSimpleSchema(schemaId, null, null));
+
+        // Now that the schema is created, run the update test.
+        testVersionConflict(1, null);
+    }
+
+    @Test
+    public void createSchemaVersionConflictWithDdbVersion() {
+        // Create the schema first.
+        developerClient.createOrUpdateUploadSchema(makeSimpleSchema(schemaId, null, null));
+
+        // This time, we add the DDB version parameter (since this now exists in DDB) to make sure we don't croak when
+        // this is present.
+        testVersionConflict(1, 1L);
+    }
+
+    private void testVersionConflict(Integer rev, Long version) {
+        UploadSchema schema = makeSimpleSchema(schemaId, rev, version);
+
+        // Create/update the schema and verified it was created/updated.
+        UploadSchema createdSchema = developerClient.createOrUpdateUploadSchema(schema);
+        assertNotNull(createdSchema);
+
+        // Create/update again. This one should throw.
+        try {
+            developerClient.createOrUpdateUploadSchema(schema);
+            fail("expected exception");
+        } catch (ConcurrentModificationException ex) {
+            // expected exception
+        }
+    }
+
+    // Helper to make an upload schema with the minimum of attributes. Takes in rev and version to facilitate testing
+    // create vs update and handling version conflicts.
+    private static UploadSchema makeSimpleSchema(String schemaId, Integer rev, Long version) {
+        UploadFieldDefinition fieldDef = new UploadFieldDefinition.Builder().withName("field")
+                .withType(UploadFieldType.STRING).build();
+        UploadSchema schema = new UploadSchema.Builder().withFieldDefinitions(fieldDef).withName("Schema")
+                .withRevision(rev).withSchemaId(schemaId).withSchemaType(UploadSchemaType.IOS_DATA)
+                .withVersion(version).build();
+        return schema;
     }
 
     @Test(expected=UnauthorizedException.class)
