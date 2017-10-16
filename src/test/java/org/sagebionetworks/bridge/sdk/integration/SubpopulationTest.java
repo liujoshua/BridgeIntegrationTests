0d5baae47cb5c150b113c81889e5116fadc36690
Alx Dark <alx.dark@sagebase.org>
2017-10-16 12:06:37 -0700

    Remove @Ignore

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/SubpopulationTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/SubpopulationTest.java
@@ -10,7 +10,6 @@ import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.sagebionetworks.bridge.rest.ClientManager;
