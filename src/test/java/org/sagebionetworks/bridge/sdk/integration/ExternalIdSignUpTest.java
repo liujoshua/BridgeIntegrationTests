ad8c1651b27ea080ccedf10e5c0995a7a20670ef
Alx Dark <alx.dark@sagebase.org>
2018-04-19 10:15:49 -0700

    Test of external ID signup.

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/ExternalIdSignUpTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/ExternalIdSignUpTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.stream.Collectors;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,13 +24,15 @@ import org.sagebionetworks.bridge.rest.model.UserSessionInfo;
 import org.sagebionetworks.bridge.rest.model.VersionHolder;
 import org.sagebionetworks.bridge.sdk.integration.TestUserHelper.TestUser;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 
 public class ExternalIdSignUpTest {
-
+    
+    private String externalId;
+    private String otherExternalId;
     private TestUser admin;
     private TestUser developer;
-    private ExternalIdentifiersApi extIdsClient;
+    private ExternalIdentifiersApi devIdsClient;
     private ForAdminsApi adminClient;
     private AuthenticationApi authClient;
     private UserSessionInfo info;
@@ -41,17 +41,14 @@ public class ExternalIdSignUpTest {
     @Before
     public void before() throws Exception {
         admin = TestUserHelper.getSignedInAdmin();
-        adminClient = admin.getClient(ForAdminsApi.class);
-        changeExternalIdValidation(false);
         developer = TestUserHelper.createAndSignInUser(ExternalIdSignUpTest.class, false, Role.DEVELOPER);
-        extIdsClient = developer.getClient(ExternalIdentifiersApi.class);
-        authClient = developer.getClient(AuthenticationApi.class);
+        externalId = Tests.randomIdentifier(ExternalIdSignUpTest.class);
+        otherExternalId = Tests.randomIdentifier(ExternalIdSignUpTest.class);
         
-        ExternalIdentifierList list = extIdsClient.getExternalIds(null, 50, null, null).execute().body();
-        if (!list.getItems().isEmpty()) {
-            extIdsClient.deleteExternalIds(list.getItems().stream()
-                    .map(ExternalIdentifier::getIdentifier).collect(Collectors.toList())).execute();
-        }
+        adminClient = admin.getClient(ForAdminsApi.class);
+        devIdsClient = developer.getClient(ExternalIdentifiersApi.class);
+        authClient = TestUserHelper.getNonAuthClient(AuthenticationApi.class, Tests.STUDY_ID);
+        changeExternalIdValidation(false);
     }
     
     @After
@@ -83,40 +80,45 @@ public class ExternalIdSignUpTest {
     
     @Test
     public void externalIdSignInTest() throws Exception {
-        // Enable validation, and an account with just an external ID will succeed
-        changeExternalIdValidation(true);
-        extIdsClient.addExternalIds(Lists.newArrayList("AAA", "BBB")).execute();
-        
-        SignUp signUp = new SignUp().study(Tests.STUDY_ID).password(Tests.PASSWORD);
-        signUp.externalId("AAA");
-        authClient.signUp(signUp).execute();
-        
-        ExternalIdentifierList list = extIdsClient.getExternalIds(null, 50, null, null).execute().body();
-        for (ExternalIdentifier identifier : list.getItems()) {
-            if ("AAA".equals(identifier.getIdentifier())) {
-                assertTrue(identifier.getAssigned());        
-            }
-        }
-        
-        // Prove you can sign in with this account (status = enabled)
-        SignIn signIn = new SignIn().externalId("AAA").study(Tests.STUDY_ID).password(Tests.PASSWORD);
         try {
-            authClient.signInV4(signIn).execute().body();    
-        } catch(ConsentRequiredException e) {
-            info = e.getSession();
-            assertEquals("AAA", info.getExternalId());
-            assertEquals(AccountStatus.ENABLED, info.getStatus());
-        }
-        
-        // With validation disabled, this kind of account will not succeed, even if the external ID is there
-        changeExternalIdValidation(false); 
-        try {
-            signUp = new SignUp().study(Tests.STUDY_ID).password(Tests.PASSWORD);
-            signUp.externalId("BBB");
+            // Enable validation, and an account with just an external ID will succeed
+            changeExternalIdValidation(true);
+            devIdsClient.addExternalIds(ImmutableList.of(externalId, otherExternalId)).execute();
+            
+            SignUp signUp = new SignUp().study(Tests.STUDY_ID).password(Tests.PASSWORD);
+            signUp.externalId(externalId);
             authClient.signUp(signUp).execute();
-            fail("Should have thrown exception.");
-        } catch(UnauthorizedException e) {
-            assertEquals("External ID management is not enabled for this study", e.getMessage());
+            
+            ExternalIdentifierList list = devIdsClient.getExternalIds(null, 50, null, null).execute().body();
+            for (ExternalIdentifier identifier : list.getItems()) {
+                if (externalId.equals(identifier.getIdentifier())) {
+                    assertTrue(identifier.getAssigned());        
+                }
+            }
+            
+            // Prove you can sign in with this account (status = enabled)
+            SignIn signIn = new SignIn().externalId(externalId).study(Tests.STUDY_ID).password(Tests.PASSWORD);
+            try {
+                authClient.signInV4(signIn).execute().body();
+                fail("Should have thrown exception.");
+            } catch(ConsentRequiredException e) {
+                info = e.getSession();
+                assertEquals(externalId, info.getExternalId());
+                assertEquals(AccountStatus.ENABLED, info.getStatus());
+            }
+            
+            // With validation disabled, this kind of account will not succeed, even if the external ID is there
+            changeExternalIdValidation(false); 
+            try {
+                signUp = new SignUp().study(Tests.STUDY_ID).password(Tests.PASSWORD);
+                signUp.externalId(otherExternalId);
+                authClient.signUp(signUp).execute();
+                fail("Should have thrown exception.");
+            } catch(UnauthorizedException e) {
+                assertEquals("External ID management is not enabled for this study", e.getMessage());
+            }
+        } finally {
+            devIdsClient.deleteExternalIds(ImmutableList.of(externalId, otherExternalId)).execute();
         }
     }
 }
