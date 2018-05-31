4e5972cd47d10e055edc51f99a7ab5888a752838
Alx Dark <alx.dark@sagebase.org>
2018-05-31 12:53:28 -0700

    Change the way you create user to verify that ITP consents the account when you sign in.

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/IntentToParticipateTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/IntentToParticipateTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.bridge.sdk.integration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.joda.time.LocalDate;
 import org.junit.After;
@@ -13,14 +14,11 @@ import org.sagebionetworks.bridge.rest.RestUtils;
 import org.sagebionetworks.bridge.rest.api.AuthenticationApi;
 import org.sagebionetworks.bridge.rest.api.ForAdminsApi;
 import org.sagebionetworks.bridge.rest.api.IntentToParticipateApi;
-import org.sagebionetworks.bridge.rest.api.ParticipantsApi;
-import org.sagebionetworks.bridge.rest.model.AccountStatus;
-import org.sagebionetworks.bridge.rest.model.AccountSummaryList;
 import org.sagebionetworks.bridge.rest.model.ConsentSignature;
+import org.sagebionetworks.bridge.rest.model.ConsentStatus;
 import org.sagebionetworks.bridge.rest.model.IntentToParticipate;
 import org.sagebionetworks.bridge.rest.model.Role;
 import org.sagebionetworks.bridge.rest.model.SharingScope;
-import org.sagebionetworks.bridge.rest.model.SignIn;
 import org.sagebionetworks.bridge.rest.model.SignUp;
 import org.sagebionetworks.bridge.rest.model.UserSessionInfo;
 import org.sagebionetworks.bridge.user.TestUserHelper;
@@ -29,7 +27,6 @@ import org.sagebionetworks.bridge.util.IntegTestUtils;
 
 public class IntentToParticipateTest {
     private TestUser admin;
-    private UserSessionInfo session;
     private TestUser researcher;
     
     @Before
@@ -45,56 +42,57 @@ public class IntentToParticipateTest {
             researcher.signOutAndDeleteUser();
         }
     }
-    @After
-    public void deleteUser() throws Exception {
-        if (session != null) {
-            ForAdminsApi adminApi = admin.getClient(ForAdminsApi.class);
-            adminApi.deleteUser(session.getId()).execute();
-        }
-    }
 
     @Test
     public void testIntentToParticipate() throws Exception {
-        ConsentSignature sig = new ConsentSignature()
-                .name("Test User")
-                .scope(SharingScope.ALL_QUALIFIED_RESEARCHERS)
-                .birthdate(LocalDate.parse("1980-01-01"));
-        
-        IntentToParticipate intent = new IntentToParticipate()
-                .phone(IntegTestUtils.PHONE)
-                .studyId(IntegTestUtils.STUDY_ID)
-                .subpopGuid(IntegTestUtils.STUDY_ID)
-                .osName("iPhone")
-                .consentSignature(sig);
-        
-        Config config = new Config();
-        String baseUrl = ClientManager.getUrl(config.getEnvironment());
-        String clientInfo = RestUtils.getUserAgent(admin.getClientManager().getClientInfo());
-        String lang = RestUtils.getAcceptLanguage(admin.getClientManager().getAcceptedLanguages());
-        
-        ApiClientProvider provider = new ApiClientProvider(baseUrl, clientInfo, lang, IntegTestUtils.STUDY_ID);
-        
-        IntentToParticipateApi intentApi = provider.getClient(IntentToParticipateApi.class);
-        
-        intentApi.submitIntentToParticipate(intent).execute();
-        
-        AuthenticationApi authApi = provider.getClient(AuthenticationApi.class);
-
-        String email = IntegTestUtils.makeEmail(IntentToParticipateTest.class);
-        
-        SignUp signUp = new SignUp()
-                .study(IntegTestUtils.STUDY_ID)
-                .phone(IntegTestUtils.PHONE)
-                .email(email)
-                .password(Tests.PASSWORD)
-                .checkForConsent(true);
-        authApi.signUp(signUp).execute().body();
-        
-        // You can no longer enable a user to sign in without also verifying email or phone number
-        ParticipantsApi participantsApi = researcher.getClient(ParticipantsApi.class);
-        AccountSummaryList list = participantsApi.getParticipants(0, 5, email, null, null, null).execute().body();
-        assertEquals(1, list.getItems().size());
-        assertEquals(AccountStatus.UNVERIFIED, list.getItems().get(0).getStatus());
+        TestUser user = null;
+        try {
+            ConsentSignature sig = new ConsentSignature()
+                    .name("Test User")
+                    .scope(SharingScope.ALL_QUALIFIED_RESEARCHERS)
+                    .birthdate(LocalDate.parse("1980-01-01"));
+            
+            IntentToParticipate intent = new IntentToParticipate()
+                    .phone(IntegTestUtils.PHONE)
+                    .studyId(IntegTestUtils.STUDY_ID)
+                    .subpopGuid(IntegTestUtils.STUDY_ID)
+                    .osName("iPhone")
+                    .consentSignature(sig);
+            
+            Config config = new Config();
+            String baseUrl = ClientManager.getUrl(config.getEnvironment());
+            String clientInfo = RestUtils.getUserAgent(admin.getClientManager().getClientInfo());
+            String lang = RestUtils.getAcceptLanguage(admin.getClientManager().getAcceptedLanguages());
+            
+            ApiClientProvider provider = new ApiClientProvider(baseUrl, clientInfo, lang, IntegTestUtils.STUDY_ID);
+            
+            IntentToParticipateApi intentApi = provider.getClient(IntentToParticipateApi.class);
+            intentApi.submitIntentToParticipate(intent).execute();
+            
+            SignUp signUp = new SignUp()
+                    .study(IntegTestUtils.STUDY_ID)
+                    .phone(IntegTestUtils.PHONE)
+                    .password(Tests.PASSWORD)
+                    .checkForConsent(true);
+            user = new TestUserHelper.Builder(IntentToParticipate.class)
+                .withSignUp(signUp)
+                .withConsentUser(false) // important, the ITP must do this.
+                .createUser();
+            
+            user.signOut();
+            AuthenticationApi authApi = provider.getClient(AuthenticationApi.class);
+            
+            // This does not throw a consent exception.
+            UserSessionInfo session = authApi.signInV4(user.getSignIn()).execute().body();
+            assertEquals(SharingScope.ALL_QUALIFIED_RESEARCHERS, session.getSharingScope());
+            
+            ConsentStatus status = session.getConsentStatuses().get(IntegTestUtils.STUDY_ID);
+            assertTrue(status.isConsented());
+        } finally {
+            if (user != null) {
+                user.signOutAndDeleteUser();
+            }
+        }
     }
     
 }
