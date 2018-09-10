27f938d08a6a7a5fb738281603b6744de9e62422
Alx Dark <alx.dark@sagebase.org>
2018-09-10 13:07:32 -0700

    Remove import.

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/ConsentTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/ConsentTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
