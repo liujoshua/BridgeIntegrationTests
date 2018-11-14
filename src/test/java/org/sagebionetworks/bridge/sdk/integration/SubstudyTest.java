1ad950de9eb28496eca5d8ae2d169585098b18bc
Alx Dark <alx.dark@sagebase.org>
2018-11-13 16:30:29 -0800

    Typo

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/SubstudyTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/SubstudyTest.java
@@ -118,7 +118,7 @@ public class SubstudyTest {
     }
     
     @Test
-    public void usersAndTaintedBySubstudyAssociation() throws Exception {
+    public void usersAreTaintedBySubstudyAssociation() throws Exception {
         // Create a substudy for this test.
         TestUser admin = TestUserHelper.getSignedInAdmin();
         ForAdminsApi adminApi = admin.getClient(ForAdminsApi.class);
