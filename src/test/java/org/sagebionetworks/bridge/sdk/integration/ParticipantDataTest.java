ea59b2f9a9b4826c1d7147c3ae14e43fb7b87ff5
Kelly Song <ksong@Kellys-MacBook-Air.local>
2021-02-06 16:34:04 -0800

    implement workerCanCrudPaticipantData()

--- src/test/java/org/sagebionetworks/bridge/sdk/integration/ParticipantDataTest.java
+++ src/test/java/org/sagebionetworks/bridge/sdk/integration/ParticipantDataTest.java
@@ -1,60 +1,111 @@
 package org.sagebionetworks.bridge.sdk.integration;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.After;
 import org.junit.Before;
-import org.sagebionetworks.bridge.rest.api.ForSuperadminsApi;
-import org.sagebionetworks.bridge.rest.model.App;
-import org.sagebionetworks.bridge.rest.model.VersionHolder;
+import org.junit.Test;
+import org.sagebionetworks.bridge.rest.api.ForConsentedUsersApi;
+import org.sagebionetworks.bridge.rest.api.ForWorkersApi;
+import org.sagebionetworks.bridge.rest.model.ParticipantData;
+import org.sagebionetworks.bridge.rest.model.StringList;
 import org.sagebionetworks.bridge.user.TestUserHelper;
 import org.sagebionetworks.bridge.user.TestUserHelper.TestUser;
-import org.sagebionetworks.bridge.util.IntegTestUtils;
 
-import static org.sagebionetworks.bridge.rest.model.Role.DEVELOPER;
-import static org.sagebionetworks.bridge.rest.model.Role.RESEARCHER;
-import static org.sagebionetworks.bridge.util.IntegTestUtils.TEST_APP_ID;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.sagebionetworks.bridge.rest.model.Role.WORKER;
 
 public class ParticipantDataTest {
-    private TestUser admin;
-    private TestUser developer;
-    private TestUser researcher;
-    private TestUser phoneUser;
-    private TestUser emailUser;
-    private String externalId;
+
+    private static final String USER_ID = "aUserId";
+    private static final String IDENTIFIER = "anIdentifier";
+
+    private static TestUser admin;
+    private static TestUser worker;
+
+    private TestUser user;
+    private String identifier1;
+    private String identifier2;
+    private String identifier3;
 
     @Before
     public void before() throws Exception {
         admin = TestUserHelper.getSignedInAdmin();
-        developer = TestUserHelper.createAndSignInUser(ParticipantDataTest.class, false, DEVELOPER);
-        researcher = TestUserHelper.createAndSignInUser(ParticipantDataTest.class, true, RESEARCHER);
-
-        externalId = Tests.randomIdentifier(ParticipantDataTest.class);
-
-        IntegTestUtils.deletePhoneUser(); //TODO I don't understand why this line is here
-
-        ForSuperadminsApi superAdminApi = admin.getClient(ForSuperadminsApi.class);
-        App app = superAdminApi.getApp(TEST_APP_ID).execute().body();
-        if (!app.isPhoneSignInEnabled() || !app.isEmailSignInEnabled()) {
-            app.setPhoneSignInEnabled(true);
-            app.setEmailSignInEnabled(true);
-            VersionHolder keys = superAdminApi.updateApp(app.getIdentifier(), app).execute().body();
-            app.version(keys.getVersion());
-        }
+        worker = TestUserHelper.createAndSignInUser(ParticipantDataTest.class, false, WORKER);
+        identifier1 = Tests.randomIdentifier(ParticipantDataTest.class);
+        identifier2 = Tests.randomIdentifier(ParticipantDataTest.class);
+        identifier3 = Tests.randomIdentifier(ParticipantDataTest.class);
     }
 
     @After
     public void after() throws Exception {
-        if (developer != null) {
-            developer.signOutAndDeleteUser();
+        if (user != null) {
+            user.signOutAndDeleteUser();
         }
-        if (researcher != null) {
-            researcher.signOutAndDeleteUser();
-        }
-        if (phoneUser != null) {
-            phoneUser.signOutAndDeleteUser();
-        }
-        if (emailUser != null) {
-            emailUser.signOutAndDeleteUser();
+        if (worker != null) {
+            worker.signOutAndDeleteUser();
         }
     }
 
+    @Test
+    public void workerCanCrudParticipantData() throws IOException {
+        user = TestUserHelper.createAndSignInUser(ParticipantDataTest.class, true);
+
+        String userId= user.getUserId();
+        String appId = user.getAppId();
+        String offsetKey = null;
+        Integer pageSize = 10;
+        ForWorkersApi workersApi = worker.getClient(ForWorkersApi.class);
+
+        // worker can create participant data
+        workersApi.saveDataForAdminWorker(appId, userId, identifier1, createParticipantData("foo", "A")).execute();
+        workersApi.saveDataForAdminWorker(appId, userId, identifier2, createParticipantData("bar", "B")).execute();
+        workersApi.saveDataForAdminWorker(appId, userId, identifier3, createParticipantData("baz", "C")).execute();
+
+        // user can get those participant data
+        ForConsentedUsersApi usersApi = user.getClient(ForConsentedUsersApi.class);
+
+        StringList results = usersApi.getAllDataForSelf(offsetKey, pageSize).execute().body();
+        assertEquals(3, results.getItems().size());
+        HashSet<String> expectedIdentifiers = new HashSet<>(Arrays.asList(identifier1, identifier2, identifier3));
+        HashSet<String> actualIdentifiers = new HashSet<>(results.getItems());
+        assertFalse(actualIdentifiers.retainAll(expectedIdentifiers));
+
+        // worker can get those participant data
+        results = workersApi.getAllDataForAdminWorker(appId, userId, offsetKey, pageSize).execute().body();
+        assertEquals(3, results.getItems().size());
+        actualIdentifiers = new HashSet<>(results.getItems());
+        assertFalse(actualIdentifiers.retainAll(expectedIdentifiers));
+    }
+
+    @Test
+    public void correctExceptionsOnBadRequest() {
+
+    }
+
+    @Test
+    public void userCanCrudSelfData() {
+
+    }
+
+    @Test
+    public void participantDataNotVisibleOutsideOfStudy() {
+
+    }
+
+    private static ParticipantData createParticipantData(String fieldValue1, String fieldValue2) {
+        ObjectNode node = JsonNodeFactory.instance.objectNode();
+        node.put("field1", fieldValue1);
+        node.put("field2", fieldValue2);
+        ParticipantData participantData = new ParticipantData();
+        participantData.setUserId(USER_ID);
+        participantData.setIdentifier(IDENTIFIER);
+        participantData.setData(String.valueOf(node));
+        return participantData;
+    }
 }
